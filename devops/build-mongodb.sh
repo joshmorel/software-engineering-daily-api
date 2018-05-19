7df2b020cb5386a79620fae4b7987456cfba454b
Josh Morel <morel.josh@hotmail.com>
2018-05-19 07:41:20 -0400

    WIP softwaredaily/sedaily-mongo

--- devops/build-mongodb.sh
+++ devops/build-mongodb.sh
@@ -6,15 +6,18 @@ if [ -z "$MONGO_DB" ]; then
 fi
 
 # download the latest backup from MongoDB
-curl -Ls -O https://s3-us-west-2.amazonaws.com/sedaily-mongo-backup/$MONGO_DB.tar.gz
+curl -Ls -O https://s3.ca-central-1.amazonaws.com/sedaily-mongo-backup2/$MONGO_DB.tar.gz
 
 # unpack and move the dump to the right directory
+rm -r dump/
 tar -zxf $MONGO_DB.tar.gz
 mkdir dump/
 mv $MONGO_DB/* dump/
 rmdir $MONGO_DB
 rm $MONGO_DB.tar.gz
 
-docker build -f mongo.Dockerfile -t andrewlloyd/sedaily-mongo .
+docker build -f mongo.Dockerfile -t softwaredaily/sedaily-mongo .
 
-#docker push andrewlloyd/sedaily-mongo
+rm -r dump/
+
+#docker push softwaredaily/sedaily-mongo
